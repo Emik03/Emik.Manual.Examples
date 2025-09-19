2a0b53bbb066a6288484d9c9f8c257117db7b86b
Emik <emik.cuber@gmail.com>
2025-09-19 20:26:59 +0200

    Add more logic variety

--- VibRibbon/VibRibbon.cs
+++ VibRibbon/VibRibbon.cs
@@ -14,17 +14,47 @@ static IAsyncEnumerable<SplitMemory<char, char, MatchOne>> Read([Match("^[^<>:\"
 }
 
 World world = new();
+Dictionary<string, IList<Item>> lookup = [];
+IList<ReadOnlyMemory<char>> varietySongs = [];
+var varietyCategory = world.Category("Variety");
 
 await foreach (var (category, (song, _)) in Read("CustomSongs.csv"))
 {
+    if (category.Span is "Variety")
+    {
+        varietySongs.Add(song);
+        continue;
+    }
+
     var c = world.Category(category);
-    var i = world.Item(song, categories: c);
-    world.Location(song, category.Span is "Song Link" or "Variety" ? i : world.AllItemsWith(c).And(), c);
+
+    var early = category.Span is not "Hello (BPM)" &&
+        (category.Span is "Song Link" || world.AllItemsWith(category).Skip(1).Any())
+            ? 0
+            : 1;
+
+    var i = world.Item(song, null, c, early: early);
+    world.Location(song, category.Span is "Song Link" ? i : world.AllItemsWith(c).And(), c);
 }
+
+for (var i = 0; i < varietySongs.Count; i++)
+{
+    IList<string> take = [..varietySongs.Concat(varietySongs).Skip(i).Take(3).Select(x => x.ToString())];
+    var it = world.Item(take.Conjoin(" & "), null, varietyCategory, early: i >= 2 ? 0 : 1);
+
+    foreach (var x in take)
+    {
+        lookup.TryAdd(x, []);
+        lookup[x].Add(it);
+    }
+}
+
+foreach (var (key, value) in lookup)
+    world.Location(key, value.Or(), varietyCategory);
 
 var finale = world.AllCategories["Finale"];
 world.Location("Complete all finale songs", finale[world.AllItemsWith("Finale").Count()], finale);
 
-await world.Game("VibRibbon", "Emik", "Play a random song, no reward (Trap)", [new(world.AllCategories["Variety"], 2)])
+await world.Game("VibRibbon", "Emik", "Play a random song, no reward (Trap)", [new(world.AllCategories["Variety"], 1)])
    .DisplayExported(Console.WriteLine)
    .ZipAsync(Path.GetTempPath(), listChecks: true);
